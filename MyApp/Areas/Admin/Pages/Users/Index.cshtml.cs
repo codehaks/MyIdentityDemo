ee9ee97967b2e0aae5816d0067edbaf55134cbf0
Hakim Ghods <ghods.hakim@hotmail.com>
2019-05-12 20:51:38 +0430

    Users

---
+++ MyApp/Areas/Admin/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyApp.Models;
+
+namespace MyApp.Areas.Admin.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public IndexModel(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IList<ApplicationUser> UserList { get; set; }
+        public void OnGet()
+        {
+            UserList = _userManager.Users.ToList();
+        }
+    }
+}
