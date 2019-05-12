f9c993d2ff072cd4fa10261ecd9917dd0dd16ad2
Hakim Ghods <ghods.hakim@hotmail.com>
2019-05-12 21:22:39 +0430

    Fix

--- MyApp/Startup.cs
+++ MyApp/Startup.cs
@@ -44,7 +44,12 @@ namespace MyApp
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc()
+                .AddRazorPagesOptions(options =>
+                {
+                    options.Conventions.AuthorizeAreaFolder("User", "/");
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
