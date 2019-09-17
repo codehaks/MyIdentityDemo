98b4126148eaefc123f2471e56dfa9f727b4b75f
Pized <vegazed7@gmail.com>
2019-09-17 12:46:58 +0430

    Claims

---
+++ MyApp/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+
+namespace MyApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Route("api/user/claims")]
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var claims = await _userManager.GetClaimsAsync(user);
+
+            return Ok(claims.Select(c =>
+            new
+            {
+                c.Type,
+                c.Value
+            }));
+
+        }
+    }
+}
